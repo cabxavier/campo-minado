8482f98f06c3995e908d941a432df87de18b22a0
Cesar <cesaraugustobarbosaxavier@gmail.com>
2023-11-14 11:55:48 -0300

    Third commit

--- src/br/com/cesar/cm/modelo/Campo.java
+++ src/br/com/cesar/cm/modelo/Campo.java
@@ -3,24 +3,72 @@ package br.com.cesar.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cesar.cm.excecao.ExplosaoException;
+
 public class Campo {
 
 	private final int linha;
 	private final int coluna;
-	
+
 	private boolean aberto = false;
 	private boolean minado = false;
 	private boolean marcado = false;
-	
+
 	private List<Campo> vizinhos = new ArrayList<>();
-	
-	Campo(int linha, int coluna){
+
+	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
-	
+
 	boolean adicionarVizinho(Campo vizinho) {
-		return false;
+		boolean linhaDiferente = this.linha != vizinho.linha;
+		boolean colunaDiferente = this.coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+
+		if (deltaGeral == 1 && !diagonal) {
+			this.vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			this.vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
+	void alternarMarcacao() {
+		if (!this.aberto) {
+			this.marcado = !this.marcado;
+		}
+	}
+
+	boolean abrir() {
+
+		if (!this.aberto && this.marcado) {
+			this.aberto = true;
+
+			if (this.minado) {
+				throw new ExplosaoException();
+			}
+
+			if (this.vizinhaSegura()) {
+				this.vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+			
+		}else {
+		
+			return false;
+		}		
+	}
+
+	boolean vizinhaSegura() {
+		return this.vizinhos.stream().noneMatch(v -> v.minado);
+	}
 }
